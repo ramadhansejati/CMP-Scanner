d139dc2a402f96d09b8b946ab7ccc426e7c1816a
Ramadhan Purbo Sejati <ramadhanpurbosejati0@gmail.com>
2024-01-05 10:06:36 +0700

    feat: init project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "CMP-Scanner"
+includeBuild("convention-plugins")
+include(":shared")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
